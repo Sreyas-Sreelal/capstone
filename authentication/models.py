fb9d14de0485df3bb81f588e1a666da02833843c
SyS <sreyassreelal@gmail.com>
2024-11-02 16:53:03 +0530

    formatted

--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 import classroom
 # Create your models here.
 
+
 class User(AbstractBaseUser):
     user_id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=100)
@@ -11,9 +12,7 @@ class User(AbstractBaseUser):
     email_id = models.EmailField(unique=True)
     password = models.CharField(max_length=72)
 
+
 class Certifications(models.Model):
-    user_id = models.ForeignKey('User',on_delete=models.CASCADE)
-    course_name = models.CharField(max_length=200,unique=True)
-
-
-
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=200, unique=True)
