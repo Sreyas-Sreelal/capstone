4750a11b0dfa1d058d53a071e9e4512573c1b5f1
SyS <sreyassreelal@gmail.com>
2024-11-02 17:13:22 +0530

    fix Classroom and Authentication models, made migrations

--- authentication/models.py
+++ authentication/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-import classroom
 # Create your models here.
 
 
 class User(AbstractBaseUser):
     user_id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=100)
-    class_id = models.ForeignKey(classroom.models.Classroom)
+    class_id = models.ForeignKey('classroom.Classroom',on_delete=models.SET_NULL,null=True)
     role = models.CharField(max_length=200)
     email_id = models.EmailField(unique=True)
     password = models.CharField(max_length=72)
--- classroom/models.py
+++ classroom/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-import authentication
 
 class Classroom(models.Model):
     class_id = models.UUIDField(primary_key=True)
     # member = models.CharField()
     #empid of the trainer
-    trainer_id = models.ForeignKey(authentication.models.User)
+    trainer_id = models.ForeignKey('authentication.User',on_delete=models.SET_NULL,null=True)
 
 class ClassMember(models.Model):
-    class_id = models.ForeignKey(Classroom)
-    member = models.ForeignKey(authentication.models.User)
+    class_id = models.ForeignKey(Classroom,on_delete=models.CASCADE)
+    member = models.ForeignKey('authentication.User',on_delete=models.SET_NULL,null=True)
 
 class ClassCourse(models.Model):
     sno = models.AutoField(primary_key=True)
-    class_id = models.ForeignKey(Classroom)
+    class_id = models.ForeignKey(Classroom,on_delete=models.CASCADE)
     
