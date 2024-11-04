cb997fc8055e58a89dd3f2b17ad53749947fce51
SyS <sreyassreelal@gmail.com>
2024-11-04 20:16:31 +0530

    course creation logic

--- course/views.py
+++ course/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import AccessToken
 from . import models
+from . import serializer
+
 # Create your views here.
 
 
@@ -23,18 +25,21 @@ def create_course(request: Request):
                     "chapters":[
                         {
                             "chapter_name":"",
-                            
-                            "questions":[
+                            "assessments":[
                                 {
-                                    "question":"",
-                                    "option_1:"",
-                                    "option_2:"",
-                                    "option_3:"",
-                                    "option_4:"",
-                                    "correct":""
+                                    "questions":[
+                                        {
+                                            "question":"",
+                                            "option_1":"",
+                                            "option_2":"",
+                                            "option_3":"",
+                                            "option_4":"",
+                                            "correct":""
+                                        }
+                                    ]
                                 }
                             ]
-                        
+
                         }
                     ]
                 }
@@ -52,29 +57,34 @@ def create_course(request: Request):
             deliverables=data['deliverables']
         )
         new_course.save()
+
         for module in data['modules']:
-            new_module = models.Module(
+            new_module = models.Module.objects.create(
                 module_name=module['module_name'],
-                course_id=new_course
             )
-            new_module.save()
+            new_course.modules.add(new_module)
+
             for chapter in module['chapters']:
-                new_chapter = models.Chapter(
-                    module_id=new_module,
-                    chapter_name=chapter['chapter_name'],   
+                new_chapter = models.Chapter.objects.create(
+                    chapter_name=chapter['chapter_name'],
                 )
-                new_chapter.save()
-                for question in chapter['questions']:
-                    new_assessment = models.Assesment(
-                        chapter_id=new_chapter,
-                        question=question["question"],
-                        option_1 = question["option_1"],
-                        option_2 = question["option_2"],
-                        option_3 = question["option_3"],
-                        option_4 = question["option_4"],
-                        correct_option = question["correct_option"],
-                    )
-                    new_assessment.save()
-        return Response({"ok":True})
+                new_module.chapters.add(new_chapter)
+
+                for assessments in chapter['assessments']:
+                    new_assessment = models.Assesment.objects.create()
+                    new_chapter.assessments.add(new_assessment)
+
+                    for question in assessments['questions']:
+                        new_question = models.Questions.objects.create(
+                            question=question["question"],
+                            option_1=question["option_1"],
+                            option_2=question["option_2"],
+                            option_3=question["option_3"],
+                            option_4=question["option_4"],
+                            correct_option=question["correct_option"],
+                        )
+                        new_assessment.questions.add(new_question)
+
+        return Response({"ok": True, "course": serializer.CourseSerializer(new_course).data})
     except Exception as e:
-        return Response({"ok":False,"error":str(e)})
+        return Response({"ok": False, "error": str(e)})
