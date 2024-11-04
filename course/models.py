3226cbb329bc37c2610586e25cdff1e430241e29
SyS <sreyassreelal@gmail.com>
2024-11-04 20:14:32 +0530

    refactored the models, use django's ManyToManyFields for easy construction of multiple relations

--- course/models.py
+++ course/models.py
@@ -1,34 +1,8 @@
 from django.db import models
 import uuid
 
-
-class Course(models.Model):
-    course_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    course_name = models.TextField(unique=True)
-    course_type = models.CharField(max_length=20)
-    deliverables = models.CharField(max_length=20)
-
-
-class Module(models.Model):
-    module_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    module_name = models.CharField(max_length=50)
-    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-
-class Chapter(models.Model):
-    chapter_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    module_id = models.ForeignKey(Module, on_delete=models.CASCADE)
-    chapter_name = models.CharField(max_length=50)
-    chapter_content = models.TextField()
-    # check
-    # What if one chapter has more than one asst?
-    # FK
-
-
-class Assesment(models.Model):
-    # PK
+class Questions(models.Model):
     question_id = models.AutoField(primary_key=True)
-    chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     question = models.TextField()
     option_1 = models.TextField()
     option_2 = models.TextField()
@@ -36,10 +10,39 @@ class Assesment(models.Model):
     option_4 = models.TextField()
     correct_option = models.TextField()
 
+class Assesment(models.Model):
+    # PK
+    assessment_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    questions = models.ManyToManyField(Questions,related_name="assessment_questions")
+    
+
+class Chapter(models.Model):
+    chapter_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    chapter_name = models.CharField(max_length=50)
+    chapter_content = models.TextField()
+    assessments = models.ManyToManyField(Assesment,related_name="chapter_assessments")
+    # check
+    # What if one chapter has more than one asst?
+    # FK
+
+class Module(models.Model):
+    module_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    module_name = models.CharField(max_length=50)
+    chapters = models.ManyToManyField(Chapter,related_name="module_chapters")
+
+class Course(models.Model):
+    course_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    course_name = models.TextField(unique=True)
+    course_type = models.CharField(max_length=20)
+    deliverables = models.CharField(max_length=20)
+    modules = models.ManyToManyField(Module,related_name='course_modules')
+
 
 class UserProgress(models.Model):
     user_id = models.ForeignKey(
-        'authentication.User', on_delete=models.CASCADE)
+        'authentication.User', 
+        on_delete=models.CASCADE
+    )
     chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     score = models.IntegerField()
     attempted = models.BooleanField(default=False)
